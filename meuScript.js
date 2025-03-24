ffeb58122351addbbd29637e75dfdecf054268da
Felipe Magron <felipemagron@gmail.com>
2025-03-23 22:48:54 -0300

    Create meuScript.js

---
+++ meuScript.js
@@ -0,0 +1,26 @@
+
+const app = Vue.createApp({
+  data() {
+      return {
+          nome: '',
+          instituicao: '',
+          curso: '',
+          novaTarefa: '',
+          tarefas: [],
+          modoEscuro: false,
+          carteiraCompleta: false
+      };
+  },
+  methods: {
+      adicionarTarefa() {
+          if (this.novaTarefa.trim() !== '') {
+              this.tarefas.push(this.novaTarefa);
+              this.novaTarefa = '';
+          }
+      },
+      alternarEstadoCarteira() {
+          this.carteiraCompleta = !this.carteiraCompleta;
+      }
+  }
+});
+app.mount('#app');
